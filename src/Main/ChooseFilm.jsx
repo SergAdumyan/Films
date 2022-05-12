85d7db87bbe932eb74da34dbdd4e8e1cfc6eec1a
SergAdumyan <sergeyadumyan@gmail.com>
2022-05-12 13:31:14 +0400

    first commit

--- src/Main/ChooseFilm.jsx
+++ src/Main/ChooseFilm.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import data from '../data.json'
 import { useDispatch } from 'react-redux'
+import styles from './Main.module.css'
 export default function ChooseFilm() {
     const arr = Object.values(data)
     console.log(arr);
     const dispatch = useDispatch()
 
   return (
-    <div style={{display:'flex',gap:'15px',overflow:'hidden'}}>
+    <div style={{display:'flex',gap:'15px',}} className={styles.diver}>
+
         {arr[1].map((elem)=>(
             
             <div key={elem.Id} onClick={()=>dispatch({
